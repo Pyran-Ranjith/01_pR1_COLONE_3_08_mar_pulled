928c2a9a995615c5242d40ff2031e420dbeddda2
PyraN-Ranjith Jayawardana <pyranranjith@gmail.com>
2024-03-01 14:17:12 +0530

    3rd push

--- 02_git_add-alland-commit.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-::___________Set Variables______________________
-:: Origin for this repo
-:: Commit msg
-    set commit_msg="second push"
-::____________Process begin here________________________
-::echo Hear is the result_______________________
-git add .
-git commit -m %commit_msg%
